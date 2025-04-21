b16650cf88d86259ae7d1c5425ed89f1e839fac4
praneeth <praneethmahapatro46@gmail.com>
2025-04-21 11:01:08 +0530

    added some files and modified some files

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -74,4 +74,15 @@ const verfiyOrder = async (req,res) => {
     }
 }
 
-export {placeOrder,verfiyOrder}
+//user orders for front end
+const userOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder,verfiyOrder,userOrders}
