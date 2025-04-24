22c2b4e3445526317aab044a9ce11877474762af
praneeth <praneethmahapatro46@gmail.com>
2025-04-24 09:07:46 +0530

    added some files and modified some files

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,4 +85,26 @@ const userOrders = async (req,res) => {
     }
 }
 
-export {placeOrder,verfiyOrder,userOrders}
+// Listing orders for admin panel
+const listOrders = async(req,res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+// api for updating order status
+const updateStatus = async (req,res) => { 
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder,verfiyOrder,userOrders,listOrders,updateStatus}
